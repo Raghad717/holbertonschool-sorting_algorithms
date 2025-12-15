9747c5bb8206c2a1debbc43c8859ffbc136c36f8
aasiria24 <amaalmoasiri@gmail.com>
2025-12-15 08:28:53 +0000

    Fixing the lingth of the functions

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -2,46 +2,91 @@
 #include <stdlib.h>
 
 /**
+* get_max - Finds the maximum value in an array
+* @array: Array of integers
+* @size: Size of the array
+*
+* Return: Maximum value
+*/
+int get_max(int *array, size_t size)
+{
+size_t i;
+int max = 0;
+
+for (i = 0; i < size; i++)
+{
+if (array[i] > max)
+max = array[i];
+}
+return (max);
+}
+
+/**
+* build_count_array - Builds and prints the counting array
+* @array: Input array
+* @size: Size of the array
+* @max: Maximum value in array
+*
+* Return: Pointer to counting array
+*/
+int *build_count_array(int *array, size_t size, int max)
+{
+size_t i;
+int *count = malloc(sizeof(int) * (max + 1));
+
+if (!count)
+return (NULL);
+
+for (i = 0; i <= (size_t)max; i++)
+count[i] = 0;
+
+for (i = 0; i < size; i++)
+count[array[i]]++;
+
+for (i = 1; i <= (size_t)max; i++)
+count[i] += count[i - 1];
+
+print_array(count, max + 1);
+return (count);
+}
+
+/**
+* build_output_array - Builds the sorted array
+* @array: Input array
+* @output: Output array
+* @count: Counting array
+* @size: Size of the array
+*/
+void build_output_array(int *array, int *output, int *count, size_t size)
+{
+size_t i;
+
+for (i = size; i > 0; i--)
+{
+output[count[array[i - 1]] - 1] = array[i - 1];
+count[array[i - 1]]--;
+}
+}
+
+/**
 * counting_sort - Sorts an array of integers using Counting sort
 * @array: Array to sort
 * @size: Size of the array
 */
 void counting_sort(int *array, size_t size)
 {
-int *count, *output;
+int max, *count, *output;
 size_t i;
-int max = 0;
 
 if (!array || size < 2)
 return;
 
-/* Find max value */
-for (i = 0; i < size; i++)
-{
-if (array[i] > max)
-max = array[i];
-}
+max = get_max(array, size);
 
-/* Allocate count array */
-count = malloc(sizeof(int) * (max + 1));
+count = build_count_array(array, size, max);
 if (!count)
 return;
 
-for (i = 0; i <= (size_t)max; i++)
-count[i] = 0;
-
-/* Store occurrences */
-for (i = 0; i < size; i++)
-count[array[i]]++;
-
-/* Prefix sum */
-for (i = 1; i <= (size_t)max; i++)
-count[i] += count[i - 1];
-
-/* Print counting array */
-print_array(count, max + 1);
-
-/* Allocate output array */
 output = malloc(sizeof(int) * size);
 if (!output)
 {
@@ -49,14 +94,8 @@ free(count);
 return;
 }
 
-/* Build output array (stable sort) */
-for (i = size; i > 0; i--)
-{
-output[count[array[i - 1]] - 1] = array[i - 1];
-count[array[i - 1]]--;
-}
+build_output_array(array, output, count, size);
 
-/* Copy back to original array */
 for (i = 0; i < size; i++)
 array[i] = output[i];
 

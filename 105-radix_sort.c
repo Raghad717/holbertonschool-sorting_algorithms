d5bf56ab93916182ec73cfe836c875a6f78af622
aasiria24 <amaalmoasiri@gmail.com>
2025-12-15 09:01:54 +0000

    Fixing the comments

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -40,8 +40,10 @@ return;
 for (i = 0; i < size; i++)
 count[(array[i] / exp) % 10]++;
 
-/* Change count[i] so that count[i] now contains actual
-position of this digit in output[] */
+/*
+ * Change count[i] so that count[i] now contains actual
+ * position of this digit in output[]
+ */
 for (i = 1; i < 10; i++)
 count[i] += count[i - 1];
 
@@ -52,8 +54,10 @@ output[count[(array[i - 1] / exp) % 10] - 1] = array[i - 1];
 count[(array[i - 1] / exp) % 10]--;
 }
 
-/* Copy the output array to array[], so that array[] now
-contains sorted numbers according to current digit */
+/*
+ * Copy the output array to array[], so that array[] now
+ * contains sorted numbers according to current digit
+ */
 for (i = 0; i < size; i++)
 array[i] = output[i];
 
@@ -76,9 +80,11 @@ return;
 /* Find the maximum number to know number of digits */
 max = get_max(array, size);
 
-/* Do counting sort for every digit. Note that instead
-of passing digit number, exp is passed. exp is 10^i
-where i is current digit number */
+/*
+* Do counting sort for every digit. Note that instead
+* of passing digit number, exp is passed. exp is 10^i
+* where i is current digit number
+*/
 for (exp = 1; max / exp > 0; exp *= 10)
 {
 counting_sort_for_radix(array, size, exp);

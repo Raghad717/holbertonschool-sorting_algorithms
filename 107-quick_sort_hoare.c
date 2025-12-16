1b0c8a7edb207aa016dea4e33f2dd4505d501cdd
aasiria24 <amaalmoasiri@gmail.com>
2025-12-16 08:11:13 +0000

    Fixing low and high function

--- 107-quick_sort_hoare.c
+++ 107-quick_sort_hoare.c
@@ -1,24 +1,26 @@
 #include "sort.h"
 
 /**
-* swap - Swaps two integers
-* @a: First integer
-* @b: Second integer
+* swap_ints - swap two integers
+* @a: first integer
+* @b: second integer
 */
-void swap(int *a, int *b)
+void swap_ints(int *a, int *b)
 {
-int temp = *a;
+int tmp = *a;
+
 *a = *b;
-*b = temp;
+*b = tmp;
 }
 
 /**
-* hoare_partition - Partitions array using Hoare scheme
-* @array: The array to partition
-* @low: Starting index
-* @high: Ending index
-* @size: Size of array (for printing)
-* Return: The partition index
+* hoare_partition - Hoare partition scheme
+* @array: array to sort
+* @low: start index
+* @high: end index
+* @size: array size
+*
+* Return: partition index
 */
 int hoare_partition(int *array, int low, int high, size_t size)
 {
@@ -28,55 +30,50 @@ int j = high + 1;
 
 while (1)
 {
-/* Find element from left that is >= pivot */
 do {
 i++;
-} while (i <= high && array[i] < pivot);
+} while (array[i] < pivot);
 
-/* Find element from right that is <= pivot */
 do {
 j--;
-} while (j >= low && array[j] > pivot);
+} while (array[j] > pivot);
 
-/* If pointers have met or crossed */
 if (i >= j)
 return (j);
 
-/* Swap elements */
-swap(&array[i], &array[j]);
+swap_ints(&array[i], &array[j]);
 print_array(array, size);
 }
 }
 
 /**
-* quick_sort_recursive - Recursively sorts array using quick sort
-* @array: The array to sort
-* @low: Starting index
-* @high: Ending index
-* @size: Size of array (for printing)
+* quick_sort_rec - recursive quick sort
+* @array: array to sort
+* @low: start index
+* @high: end index
+* @size: array size
 */
-void quick_sort_recursive(int *array, int low, int high, size_t size)
+void quick_sort_rec(int *array, int low, int high, size_t size)
 {
-int pi;
+int p;
 
 if (low < high)
 {
-pi = hoare_partition(array, low, high, size);
-quick_sort_recursive(array, low, pi, size);
-quick_sort_recursive(array, pi + 1, high, size);
+p = hoare_partition(array, low, high, size);
+quick_sort_rec(array, low, p, size);
+quick_sort_rec(array, p + 1, high, size);
 }
 }
 
 /**
-* quick_sort_hoare - Sorts an array of integers in ascending order
-* using the Quick sort algorithm with Hoare partition scheme
-* @array: The array to be sorted
-* @size: Number of elements in the array
+* quick_sort_hoare - Quick sort using Hoare partition
+* @array: array to sort
+* @size: array size
 */
 void quick_sort_hoare(int *array, size_t size)
 {
-if (array == NULL || size < 2)
+if (!array || size < 2)
 return;
 
-quick_sort_recursive(array, 0, size - 1, size);
+quick_sort_rec(array, 0, size - 1, size);
 }
